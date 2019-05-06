e53ae6568f17b19b29a166230a007d572f829979
Colin <colin97.lee@gmail.com>
2019-05-06 15:05:30 +0800

    Data has been handled.

--- data_calc/index.js
+++ data_calc/index.js
@@ -1,15 +1,48 @@
 let fs = require('fs');
 
-let obj = JSON.parse(fs.readFileSync('GDP.json', "utf8"));
+// I CHANGED THE FILE NAME!
+let obj = JSON.parse(fs.readFileSync('source.json', "utf8"));
 
-let output = {};
+let myObject = {};
 
 obj.forEach(element => {
-    const code = element.Code;
-    
-    delete element.Code;
+    // Create new key-value pair if country doesn't exist in output object.
+    if (myObject[element.ISO] == null) {
+        myObject[element.ISO] = {
+            Mean: [],
+            Underweight: [],
+            Obesity: [],
+            Severe: [],
+            Morbid: []
+        };
+    }
 
-    output[code.toString()] = element;
+    // Convert Raw data to percentage, then round to one digit.
+    // Math.round can only round to integer. Feel bad.
+    function calc(data, isPercentage = true) {
+        if (isPercentage) {
+            return Math.round(data * 1000) / 10;
+        }
+        return Math.round(data * 10) / 10;
+    }
+
+    const targetObj = myObject[element.ISO]
+
+    // Add correspond per-year data into the output obj.
+    targetObj["Mean"].push(calc(element.Mean, false));
+    targetObj["Underweight"].push(calc(element.Underweight));
+    targetObj["Obesity"].push(calc(element.Obesity));
+    targetObj["Severe"].push(calc(element.Severe));
+    targetObj["Morbid"].push(calc(element.Morbid));
 });
 
-console.log(output.CHN);
+let outputJson = JSON.stringify(myObject);
+
+// I ALSO CHANGED THIS FILE NAME!
+fs.writeFile("output.json", outputJson, "utf8", (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    
+    console.log("Saved.");
+})
